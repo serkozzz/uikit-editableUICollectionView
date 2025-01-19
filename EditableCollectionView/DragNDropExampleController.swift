e4f915dd16548c8cf35e891ffc62df9d076d08a2
Sergey Kozlov <s.kozlov@blackmana.com>
2025-01-19 06:16:31 +0300

    drag n drop sample is based on diffableDataSource

--- EditableCollectionView/DragNDropExampleController.swift
+++ EditableCollectionView/DragNDropExampleController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 private let reuseIdentifier = "mycell"
 
-private struct IndexCard {
+private struct IndexCard : Hashable {
     var title: String
     var img: UIImage
+    var id = UUID()
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: IndexCard, rhs: IndexCard) -> Bool {
+        lhs.id == rhs.id
+    }
 }
 
 private var cards: [IndexCard] = [
@@ -24,8 +33,59 @@ private var cards: [IndexCard] = [
     IndexCard(title: "Card 7", img: UIImage(systemName: "exclamationmark.triangle.text.page.rtl")!),
     ]
 
-class DragNDropExampleController: UICollectionViewController, UICollectionViewDragDelegate, UICollectionViewDropDelegate {
+class DragNDropExampleController: UICollectionViewController {
 
+    private var dataSource: UICollectionViewDiffableDataSource<Int, IndexCard>!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        collectionView.collectionViewLayout = createLayout()
+        
+        collectionView.dragInteractionEnabled = true // Включить drag
+        collectionView.dragDelegate = self
+        collectionView.dropDelegate = self
+
+        dataSource = UICollectionViewDiffableDataSource<Int, IndexCard>(collectionView: collectionView) { [weak self] collectionView, indexPath, card in
+            
+            guard let self else { return nil }
+            var cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MyCollectionViewCell
+                
+            cell.title.text = cards[indexPath.item].title
+            cell.img.image = cards[indexPath.item].img
+        
+            return cell
+        }
+        applySnapshot()
+    }
+
+    
+    func applySnapshot() {
+        var snapshot = NSDiffableDataSourceSnapshot<Int, IndexCard>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(cards)
+        dataSource.apply(snapshot, animatingDifferences: true)
+    }
+    
+    
+    
+    func createLayout() -> UICollectionViewCompositionalLayout {
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalWidth(0.3))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.3))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
+        
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        return layout
+        
+    }
+}
+
+
+extension DragNDropExampleController: UICollectionViewDragDelegate, UICollectionViewDropDelegate {
+    
     func collectionView(_ collectionView: UICollectionView,
                         itemsForBeginning session: UIDragSession,
                         at indexPath: IndexPath) -> [UIDragItem] {
@@ -44,62 +104,9 @@ class DragNDropExampleController: UICollectionViewController, UICollectionViewDr
     func collectionView(_ collectionView: UICollectionView,
                         performDropWith coordinator: UICollectionViewDropCoordinator) {
         guard let destinationIndexPath = coordinator.destinationIndexPath else { return }
-
-        collectionView.performBatchUpdates {
-            // Удаляем элемент из старой позиции
-            let item = cards.remove(at: coordinator.items.first!.sourceIndexPath!.item)
-
-            // Вставляем элемент на новое место
-            cards.insert(item, at: destinationIndexPath.item)
-            collectionView.moveItem(at: coordinator.items.first!.sourceIndexPath!, to: destinationIndexPath)
-        }
+        let item = cards.remove(at: coordinator.items.first!.sourceIndexPath!.item)
+        // Вставляем элемент на новое место
+        cards.insert(item, at: destinationIndexPath.item)
+        applySnapshot()
     }
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        collectionView.collectionViewLayout = createLayout()
-        
-        collectionView.dragInteractionEnabled = true // Включить drag
-        collectionView.dragDelegate = self
-        collectionView.dropDelegate = self
-
-    }
-
-    func createLayout() -> UICollectionViewCompositionalLayout {
-        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3), heightDimension: .fractionalWidth(0.3))
-        let item = NSCollectionLayoutItem(layoutSize: itemSize)
-        
-        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalWidth(0.3))
-        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
-        
-        let section = NSCollectionLayoutSection(group: group)
-        
-        let layout = UICollectionViewCompositionalLayout(section: section)
-        return layout
-        
-    }
-    // MARK: UICollectionViewDataSource
-
-    override func numberOfSections(in collectionView: UICollectionView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 1
-    }
-
-
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of items
-        return cards.count
-    }
-
-    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        var cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! MyCollectionViewCell
-            
-        cell.title.text = cards[indexPath.item].title
-        cell.img.image = cards[indexPath.item].img
-    
-        return cell
-    }
-
-
 }
